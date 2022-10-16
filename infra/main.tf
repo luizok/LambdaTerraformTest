3a1ea22a22c0f4e6d5b72bf526cd90ca048ecda9
Luiz Felipe <luizfelipesmaciel@gmail.com>
2022-10-15 23:35:46 -0300

    :bricks: Added lambda role

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,25 @@
 data "archive_file" "init" {
-  type       = "zip"
-  source_dir = "../app"
+  type        = "zip"
+  source_dir  = "../app"
   output_path = "../out/code.zip"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "tf_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
